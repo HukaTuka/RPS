527b3805a6dbc0f0c85d9d30d687c98bda414fdd
jonlinnet28 <jonlinnet28@gmail.com>
2026-02-27 09:02:01 +0100

    GameViewController

--- src/rps/gui/controller/GameViewController.java
+++ src/rps/gui/controller/GameViewController.java
@@ -1,31 +1,45 @@
 package rps.gui.controller;
 
-// Java imports
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import rps.bll.game.Move;
+import rps.bll.player.IPlayer;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
-/**
- *
- * @author smsj
- */
 public class GameViewController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML public Label playerChoiceLabel;
+    @FXML public Label computerChoiceLabel;
+    @FXML public Label resultLabel;
+    @FXML public Label playerScoreLabel;
+    @FXML public Label computerScoreLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        IPlayer
+                playRound;
+        // Labels are ready to use here
     }
 
-    public void handleRock(ActionEvent actionEvent) {
+    @FXML
+    public Move handleRock(ActionEvent actionEvent) {
+        playerChoiceLabel.setText("Your Choice: Rock");
+        return Move.Rock;
     }
 
-    public void handlePaper(ActionEvent actionEvent) {
+    @FXML
+    public Move handlePaper(ActionEvent actionEvent) {
+        playerChoiceLabel.setText("Your Choice: Paper");
+        return Move.Paper;
     }
 
-    public void handleScissors(ActionEvent actionEvent) {
+    @FXML
+    public Move handleScissors(ActionEvent actionEvent) {
+        playerChoiceLabel.setText("Your Choice: Scissors ╰(*°▽°*)╯");
+        return Move.Scissor;
     }
 }
